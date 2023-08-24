930478033b1103765445722f1501f0c976065ad6
sahilanand72 <118097650+sahilanand72@users.noreply.github.com>
2023-08-24 10:37:55 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,17 @@
+const cityInput = document.querySelector(".inputter");
+const searchButton = document.querySelector(".searchBut");
+const API_KEY = "1ed0895d0fac92238eb0dc530ab761cc";
+
+const getCityCoordinates = () =>{
+    const cityName = cityInput.value.trim();
+    if(!cityName) return;
+   
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
+        console.log(data)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates")
+    })
+}
+
+searchButton.addEventListener("click",getCityCoordinates);
