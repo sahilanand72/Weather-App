c82e91e3a00ea19d1c2b24db16224002b7cf9434
sahilanand72 <118097650+sahilanand72@users.noreply.github.com>
2023-08-25 21:37:02 +0530

    Update script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -4,12 +4,25 @@ const API_KEY = "1ed0895d0fac92238eb0dc530ab761cc";
 //the api key is still not working.
 //going to try other Apis sources for the weather app
 
+const getWeatherDetails = (cityName, lat, lon) => {
+    const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+
+    fetch(WEATHER_API_URL).then(res => res.json()).then(data => {
+        console.log(data);
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!");
+    });
+}
+
 const getCityCoordinates = () =>{
     const cityName = cityInput.value.trim();
     if(!cityName) return;
    
     const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
     fetch(GEOCODING_API_URL).then(res => res.json()).then(data =>{
+        if(!data.length) return alert(`No coordinates found for ${cityName}`);
+        const{name,lat,lon} = data[0];
+        getWeatherDetails(name,lat,lon);
         console.log(data)
     }).catch(() => {
         alert("An error occured while fetching the coordinates")
