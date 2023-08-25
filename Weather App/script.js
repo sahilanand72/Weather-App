e54cd04d2694c27eb08865393b4cb2f8a4ab7d08
sahilanand72 <118097650+sahilanand72@users.noreply.github.com>
2023-08-25 21:34:02 +0530

    Update script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,6 +1,8 @@
 const cityInput = document.querySelector(".inputter");
 const searchButton = document.querySelector(".searchBut");
 const API_KEY = "1ed0895d0fac92238eb0dc530ab761cc";
+//the api key is still not working.
+//going to try other Apis sources for the weather app
 
 const getCityCoordinates = () =>{
     const cityName = cityInput.value.trim();
